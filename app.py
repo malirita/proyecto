9da5253cc51c6a64deb617448ee06b97c19908e6
Maria Lira <maliraleano@gmail.com>
2024-12-26 18:35:52 -0600

    grafico de barras 3

--- app.py
+++ app.py
@@ -8,16 +8,14 @@ car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
 st.header('Data and Vehicles')
 car_data['type'].value_counts()  # Contar la cantidad de vehículos por marca
-bar_button = st.button('Construir un gráficon de barras')
-fig = px.bar(car_data, x='type'
-             # title='Cantidad de autos más vendidos',
-             xlabel='Tipo de automóvil',
-             ylabel='Cantidad')
-fig.show()
-
-fig = px.box(car_data, x='model', y='price',
-             title="Distribución de precios por modelo")
-fig.show()
+bar_button = st.button('Construir un gráficon de barras')  # crear botón
+if bar_button:
+    # mensaje
+    st.write(
+        'Creación de un gráfico de barras para el conteo de los tipos de automóviles')
+    # grafico de barras
+    fig = px.bar(car_data, x='type')
+    st.plotly_chart(fig, use_container_width=True)
 
 hist_button = st.button('Construir histograma')  # crear un botón
 if hist_button:  # al hacer clic en el botón
