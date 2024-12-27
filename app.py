aa36a37edc32e5d383ccbab3a60bc8fb2c1a1e03
Maria Lira <maliraleano@gmail.com>
2024-12-26 18:11:09 -0600

    Try one: distribution by model

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ from pathlib import Path
 
 # lectura de datos
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+
+st.header('Data and Vehicles')
+
+fig = px.box(car_data, x='model', y='price',
+             title="Distribución de precios por modelo")
+fig.show()
+
 hist_button = st.button('Construir histograma')  # crear un botón
-
-
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
