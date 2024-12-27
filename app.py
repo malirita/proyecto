06d68539f1c51780a394bc364624b048cb83cb40
Maria Lira <maliraleano@gmail.com>
2024-12-26 18:29:13 -0600

    Grafico de barras

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
 st.header('Data and Vehicles')
 car_data['type'].value_counts()  # Contar la cantidad de vehículos por marca
-fig = px.bar(car_data, x='type')
-
+bar_button = st.button('Construir un gráficon de barras')
+fig = px.bar(car_data, x='type'
+             title='Cantidad de autos más vendidos',
+             xlabel='Tipo de automóvil',
+             ylabel='Cantidad')
 fig.show()
 
 fig = px.box(car_data, x='model', y='price',
